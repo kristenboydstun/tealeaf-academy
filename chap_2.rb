a1ebb9ef8020fe9a1f9ac105c35ccb1c1e338ad0
Kristen Boydstun <kristen.boydstun@gmail.com>
2013-09-04 22:26:22 -0700

    Exercises for chapter 2

---
+++ chap_2.rb
@@ -0,0 +1,8 @@
+puts "\nHours in a year"
+puts 365*24
+puts "\nMinutes in a decade"
+puts 10*365*24*60
+puts "\nMy age in seconds"
+puts 26*365*24*60*60
+puts "\nAuthor's age in years"
+puts 1160000000.0/60.0/60.0/24.0/365.0
