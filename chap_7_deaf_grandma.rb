1826c95a41f43ee7de395c34b2dd5dbd576380c0
Kristen Boydstun <kristen.boydstun@gmail.com>
2013-09-12 22:53:58 -0700

    Chap 7 - deaf grandma extended

--- chap_7_deaf_grandma.rb
+++ chap_7_deaf_grandma.rb
@@ -1,3 +1,5 @@
+byeCount = 0
+
 while true
 	request = gets.chomp
 
@@ -5,9 +7,16 @@ while true
 		puts 'HUH?!  SPEAK UP, SONNY!'
 	else
 		puts 'NO, NOT SINCE '+(rand(20) + 1930).to_s+'!'
+
+		if request != 'BYE'
+			byeCount = 0
+		else
+			byeCount = byeCount + 1
+		end
+
 	end
 
-	if request == 'BYE'
+	if byeCount == 3
 		break
 	end
 end
