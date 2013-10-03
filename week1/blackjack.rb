1e7d4a517436bd73a3a31c340a21d8b1cef69eb1
Kristen Boydstun <kristen.boydstun@gmail.com>
2013-10-02 19:10:56 -0700

    Fixed indentation

--- week1/blackjack.rb
+++ week1/blackjack.rb
@@ -1,19 +1,19 @@
 class Deck
   def initialize
     @suits = ["hearts","spades","diamonds","clubs"]
-  	@card_names = ["ace", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "jack", "queen", "king"]
-  	@cards = []
-  	@valueDictionary = {"ace" => 11, "one" => 1, "two" => 2, "three" => 3, "four" => 4, "five" => 5, "six" => 6, "seven" => 7, "eight" => 8, "nine" => 9, "ten" => 10, "jack" => 10, "queen" => 10, "king" => 10}
+    @card_names = ["ace", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "jack", "queen", "king"]
+    @cards = []
+    @valueDictionary = {"ace" => 11, "one" => 1, "two" => 2, "three" => 3, "four" => 4, "five" => 5, "six" => 6, "seven" => 7, "eight" => 8, "nine" => 9, "ten" => 10, "jack" => 10, "queen" => 10, "king" => 10}
 
     @suits.each do |suit|
-    	@card_names.each do |card_name|
-    		@cards << CardHolder.new(card_name, suit, @valueDictionary["#{card_name}"])
-    	end
+      @card_names.each do |card_name|
+        @cards << CardHolder.new(card_name, suit, @valueDictionary["#{card_name}"])
+      end
     end
   end  
 
   def numCards
-  	@cards.length
+    @cards.length
   end
 
   def getCards  
@@ -21,11 +21,11 @@ class Deck
   end 
 
   def shuffle
-  	@cards.shuffle!
+    @cards.shuffle!
   end
 
   def getCard
-  	@cards.pop
+    @cards.pop
   end
 end 
 
@@ -35,26 +35,26 @@ end
 
 class Player
   def initialize (name, deck)
-  	@name = name
-  	@deck = deck
-  	@cards = []
+    @name = name
+    @deck = deck
+    @cards = []
   end
 
   def greeting
     "Welcome " + @name
   end
   def showHand
-  	@cards
+    @cards
   end
   def countCards
-  	total = 0
-  	@cards.each do |card| 
-  		total += card.value
-  	end
-  	total
+    total = 0
+    @cards.each do |card| 
+      total += card.value
+    end
+    total
   end
   def hit
-  	@cards << @deck.getCard
+    @cards << @deck.getCard
   end
 end
 
