a7043683a665fbdb5737f7c866e61f703b84edd8
Kristen Boydstun <kristen.boydstun@gmail.com>
2013-10-06 23:45:21 -0700

    Fixed sort criteria

--- week1/chap_10_nonrecursive_sort.rb
+++ week1/chap_10_nonrecursive_sort.rb
@@ -4,7 +4,7 @@ sorted_list = []
 while !unsorted_list.empty?
   smallest = unsorted_list[0]
   unsorted_list.each do |element| 
-    smallest = element if element[0] < smallest[0]
+    smallest = element if element < smallest
   end
   sorted_list << unsorted_list.delete_at(unsorted_list.index(smallest))
 end
